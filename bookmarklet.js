6b8f5057d45c5a79c1f7b5fe3f59358536392255
Johann Garces <Johann1356@gmail.com>
2015-01-11 19:44:36 -0500

    Added .trim()
    just a little cleaner

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){window.prompt("Page Title. Ctl+C to copy.",document.getElementsByTagName("title")[0].innerText);})();
+javascript:(function(){window.prompt("Page Title. Ctl+C to copy.",document.getElementsByTagName("title")[0].innerText.trim());})();
